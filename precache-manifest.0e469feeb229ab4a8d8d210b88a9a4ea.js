351a27d1703db87fbad28d47f42f0d42ac85a747
Cristian Duta <cristian.duta90@gmail.com>
2019-10-02 09:39:30 +0200

    Updates

---
+++ precache-manifest.0e469feeb229ab4a8d8d210b88a9a4ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e555feef2d8ebdd9a87b4375cee63b88",
+    "url": "/dictionary-manager/index.html"
+  },
+  {
+    "revision": "d0f8675949007b73dfbf",
+    "url": "/dictionary-manager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d9f4f8c6c3c5eb269562",
+    "url": "/dictionary-manager/static/js/2.e1c9e762.chunk.js"
+  },
+  {
+    "revision": "d0f8675949007b73dfbf",
+    "url": "/dictionary-manager/static/js/main.b9cbf84f.chunk.js"
+  },
+  {
+    "revision": "4bdebfe9c0574b62e9e9",
+    "url": "/dictionary-manager/static/js/runtime-main.15881f81.js"
+  }
+]);
